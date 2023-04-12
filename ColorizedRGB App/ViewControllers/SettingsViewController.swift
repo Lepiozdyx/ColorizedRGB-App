064d5ac82e0f7e8583f58429d89625d46c221855
Alex Zhulai <Lepiozdyx@gmail.com>
2023-04-12 14:54:05 -0300

    Create a SettingsVC + protocol

--- ColorizedRGB App/ViewControllers/SettingsViewController.swift
+++ ColorizedRGB App/ViewControllers/SettingsViewController.swift
@@ -7,7 +7,11 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+protocol SettingsViewControllerDelegate: AnyObject {
+    func setBackgroundColor(for color: UIColor)
+}
+
+final class SettingsViewController: UIViewController {
     // MARK: - IB Outlets
     @IBOutlet var colorView: UIView!
     
@@ -18,11 +22,17 @@ final class ViewController: UIViewController {
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
+    
+    // MARK: - Public Properties
+    var color: UIColor!
+    unowned var delegate: SettingsViewControllerDelegate!
 
     // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorView.layer.cornerRadius = 15
+        colorView.layer.cornerRadius = colorView.frame.height / 8
+        setupSlidersValues()
+        colorView.backgroundColor = getValuesFromSliders()
         changeLabelValues(for: redSlider, label: redLabel)
         changeLabelValues(for: greenSlider, label: greenLabel)
         changeLabelValues(for: blueSlider, label: blueLabel)
@@ -38,20 +48,38 @@ final class ViewController: UIViewController {
         default:
             changeLabelValues(for: blueSlider, label: blueLabel)
         }
-        changeColorView()
+        colorView.backgroundColor = getValuesFromSliders()
+    }
+    
+    @IBAction func doneButtonTapped() {
+        let colorValue = getValuesFromSliders()
+        delegate.setBackgroundColor(for: colorValue)
+        dismiss(animated: true)
     }
     
     // MARK: - Private functions
-    private func changeColorView () {
-        colorView.backgroundColor = UIColor(
+    private func changeLabelValues(for slider: UISlider, label: UILabel) {
+        label.text = String(format: "%.2f", slider.value)
+    }
+    
+    private func setupSlidersValues() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        color.getRed(&red, green: &green, blue: &blue, alpha: nil)
+        
+        redSlider.value = Float(red)
+        greenSlider.value = Float(green)
+        blueSlider.value = Float(blue)
+    }
+    
+    private func getValuesFromSliders() -> UIColor {
+        return UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1
         )
-    }
-    
-    private func changeLabelValues(for slider: UISlider, label: UILabel) {
-        label.text = String(format: "%.2f", slider.value)
     }
 }
